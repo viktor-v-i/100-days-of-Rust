442ca874b56b6dac20c1762e674b678933b120c6
Viktor <viktor.mailmailv@gmail.com>
2026-03-10 13:10:49 +0100

    Add day-01

---
+++ src/day-01/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+   println!("fn is how you declare a function.");
+   println!("println! calls a macro, if you want to call a function remove '!'.");
+   println!("Packages of code are called 'crates'.");
+   println!("Use 'cargo new' to create a new project");
+   println!("Check if your code compiles with 'cargo check'.");
+}
